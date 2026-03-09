c2b42c39aed5439ebf03e56106d1c0613efb9d58
Koumolou <learningsin2024@gmail.com>
2026-03-09 13:07:29 +0000

    Mobile

--- ppd/src/component/AddHabitForm.tsx
+++ ppd/src/component/AddHabitForm.tsx
@@ -31,7 +31,7 @@ function AddHabitForm({ addHabit }: AddHabitFormProps) {
 
       {/* Modal */}
       {showModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="fixed inset-0 z-50 flex items-center justify-center px-4">
           {/* Backdrop */}
           <div
             className="absolute inset-0 bg-black/60 backdrop-blur-sm"
--- ppd/src/component/AddNoteForm.tsx
+++ ppd/src/component/AddNoteForm.tsx
@@ -32,7 +32,7 @@ const AddNoteForm = () => {
 
       {/* Modal */}
       {isOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="fixed inset-0 z-50 flex items-center justify-center px-4">
           {/* Backdrop */}
           <div
             className="absolute inset-0 bg-black/60 backdrop-blur-sm"
